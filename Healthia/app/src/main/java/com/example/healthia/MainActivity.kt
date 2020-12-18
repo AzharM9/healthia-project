9c79f53e2bd32624c2a18225fc037eb1310d7c37
AzharM9 <56185093+AzharM9@users.noreply.github.com>
2020-12-19 01:54:36 +0700

    menyesuaikan bottom navbar sesuai kebutuhan, tab layout sdh dibuat tp ada problem

--- Healthia/app/src/main/java/com/example/healthia/MainActivity.kt
+++ Healthia/app/src/main/java/com/example/healthia/MainActivity.kt
@@ -7,19 +7,30 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionPagerAdapter = SectionPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = sectionPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+        supportActionBar?.elevation = 0f
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_timeline, R.id.navigation_search, R.id.navigation_panicBtn, R.id.navigation_profile))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
